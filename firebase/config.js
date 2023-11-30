a7d6219c8132c6a0f11df5e92e86c0b01917d564
Rahul Gupta <rg5353070@gmail.com>
2023-11-30 18:57:53 +0530

    dasdas

--- firebase/config.js
+++ firebase/config.js
@@ -2,13 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDrw2LaXt0wOzkWkLVvYtPraomjpFSfIi0",
+  authDomain: "nextjs-techno-club.firebaseapp.com",
+  projectId: "nextjs-techno-club",
+  storageBucket: "nextjs-techno-club.appspot.com",
+  messagingSenderId: "811282710466",
+  appId: "1:811282710466:web:23f99e7496b7f3cc54205b"
 };
-
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
