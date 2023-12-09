c7ebeb54ea119a444795db75312f610c43719205
Rahul Gupta <rg5353070@gmail.com>
2023-12-09 23:03:27 +0530

    removed mdxdasadsasd

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withMDX = require('@next/mdx')()
+
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   images: {
     remotePatterns: [
       {
