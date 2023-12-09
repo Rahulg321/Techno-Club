432b329de22b176ac9c635bcdb617d7f74c6ca4c
Rahul Gupta <rg5353070@gmail.com>
2023-12-09 22:06:45 +0530

    removed libraries for deployment

--- components/ImageContainer.tsx
+++ components/ImageContainer.tsx
@@ -3,7 +3,10 @@ import { GalleryImage } from '@/app/types';
 
 const ImageContainer = (props: GalleryImage) => {
   return (
-    <div className="relative h-64 bg-static overflow-hidden group">
+    <div
+      className="relative h-64 bg-static overflow-hidden group"
+      key={props.imageId}
+    >
       <Image
         alt="some image"
         src={props.image_url}
