d14725fa1634bb337232a93cacfbd8ed9a4ccfe4
Rahul Gupta <rg5353070@gmail.com>
2024-01-08 16:37:06 +0530

    removed firebase slug blogs page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,30 +1,14 @@
-import { getBlogs } from '@/firebase/getBlogs';
-import { fetchBlogPost } from '@/firebase/getBlogPost';
-import { notFound } from 'next/navigation';
-import { MDXRemote } from 'next-mdx-remote/rsc';
-import classes from './BlogContent.module.css';
+import { getBlogs } from "@/firebase/getBlogs";
+import { fetchBlogPost } from "@/firebase/getBlogPost";
+import { notFound } from "next/navigation";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import classes from "./BlogContent.module.css";
 
 export const revalidate = 3600; // revalidate the data at most every hour
 
-export async function generateStaticParams() {
-  const posts = await getBlogs();
-
-  return posts.map((post) => ({
-    slug: post.blogId,
-  }));
-}
-
 const SingleBlog = async ({ params }: { params: { slug: string } }) => {
-  const blogPost = await fetchBlogPost(params.slug);
-
-  if (!blogPost) {
-    return notFound();
-  }
-
   return (
-    <section className={`narrow-container ${classes['blog-content']}`}>
-      <MDXRemote source={blogPost.main_content} />
-    </section>
+    <section className={`narrow-container`}>what is going on here</section>
   );
 };
 
